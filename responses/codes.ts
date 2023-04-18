e148894798e3797e9fc2b605c7d4180d39fb9eb9
Hubert Wang <hubert.tatra@gmail.com>
2023-04-18 22:08:35 +0700

    Add REST standard response

---
+++ responses/codes.ts
@@ -0,0 +1,10 @@
+export class SuccessCodes {
+    static readonly OK = 'success/ok';
+}
+
+export class ErrorCodes {
+    static readonly GENERAL_ERR = 'error/general';
+    
+    static readonly UNIMPLEMENTED_METHOD = 'error/unimplemented-method';
+    static readonly INVALID_INPUT = 'error/invalid-input';
+}
