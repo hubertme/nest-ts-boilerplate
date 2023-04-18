1f96cb658a609face309e80391fdd4f99faa07f1
Hubert Wang <hubert.tatra@gmail.com>
2023-04-18 23:37:58 +0700

    Modify NetworkingUtil

--- utils/networking_util.ts
+++ utils/networking_util.ts
@@ -20,7 +20,7 @@ export default class NetworkingUtil {
     constructor(baseURL: string, defaultHeaders: Record<string, string>) {
         const options: AxiosRequestConfig = {
             baseURL,
-            timeout: 60000, // Set timeout to 60s
+            timeout: 60 * 1000, // Set timeout to 60s - in ms
         };
         this.axiosInstance = axios.create(options);
         this.defaultHeaders = defaultHeaders;
@@ -33,49 +33,53 @@ export default class NetworkingUtil {
         }, {} as Record<string, string>);
     }
 
-    async get<T>(url: string, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
-        headers = {
-            ...this.defaultHeaders,
-            'Content-Type': 'application/json',
-            ...headers,
-        };
-        const response: AxiosResponse<T> = await this.axiosInstance.get<T>(url, {
-            headers,
-        });
-        const responseHeaders = this.convertHeaders(response.headers);
+    private async request<T>(method: string, url: string, data?: any, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
+        try {
+            headers = {
+                ...this.defaultHeaders,
+                'Content-Type': 'application/json',
+                ...headers,
+            };
+            const options: AxiosRequestConfig = {
+                url,
+                method,
+                headers,
+                data,
+            };
+            const response: AxiosResponse<T> = await this.axiosInstance.request<T>(options);
+            const responseHeaders = this.convertHeaders(response.headers);
+    
+            return new ResponseObject<T>(responseHeaders, response.data);
+        } catch (e) {
+            if (e.response) {
+                const responseHeaders = this.convertHeaders(e.response.headers);
+                const responseObject = new ResponseObject<T>(responseHeaders, e.response.data);
+                
+                return responseObject;
+            } else {
+                throw e;
+            }
+        }
+    }
 
-        return new ResponseObject<T>(responseHeaders, response.data);
+    async get<T>(url: string, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
+        return this.request<T>('GET', url, undefined, headers);
     }
 
     async post<T>(url: string, data: any, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
-        headers = {
-            ...this.defaultHeaders,
-            'Content-Type': 'application/json',
-            ...headers,
-        };
-        const response: AxiosResponse<T> = await this.axiosInstance.post<T>(url, data, {
-            headers,
-        });
-        const responseHeaders = this.convertHeaders(response.headers);
-
-        return new ResponseObject<T>(responseHeaders, response.data);
+        return this.request<T>('POST', url, data, headers);
     }
+
     async postForm<T>(url: string, formData: any, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
         headers = {
             ...this.defaultHeaders,
             'Content-Type': 'application/x-www-form-urlencoded',
             ...headers,
         };
-        const serializedData = Object.keys(formData)
-            .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(formData[key])}`)
-            .join('&');
-        const response: AxiosResponse<T> = await this.axiosInstance.post<T>(url, serializedData, {
-            headers,
-        });
-        const responseHeaders = this.convertHeaders(response.headers);
-
-        return new ResponseObject<T>(responseHeaders, response.data);
+        const serializedData = new URLSearchParams(formData);
+        return this.request<T>('post', url, serializedData.toString(), headers);
     }
+
     async postFormFile<T>(url: string, file: File, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
         const formData = new FormData();
         formData.append('file', file);
@@ -84,53 +88,18 @@ export default class NetworkingUtil {
             'Content-Type': 'multipart/form-data',
             ...headers,
         };
-        const response: AxiosResponse<T> = await this.axiosInstance.post<T>(url, formData, {
-            headers,
-        });
-        const responseHeaders = this.convertHeaders(response.headers);
-
-        return new ResponseObject<T>(responseHeaders, response.data);
+        return this.request<T>('post', url, formData, headers);
     }
 
     async put<T>(url: string, data: any, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
-        headers = {
-            ...this.defaultHeaders,
-            'Content-Type': 'application/json',
-            ...headers,
-        };
-        const response: AxiosResponse<T> = await this.axiosInstance.put<T>(url, data, {
-            headers,
-        });
-        const responseHeaders = this.convertHeaders(response.headers);
+        return this.request<T>('PUT', url, data, headers);
+    }
 
-        return new ResponseObject<T>(responseHeaders, response.data);
+    async patch<T>(url: string, data: any, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
+        return this.request<T>('PATCH', url, data, headers);
     }
 
     async delete<T>(url: string, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
-        headers = {
-            ...this.defaultHeaders,
-            'Content-Type': 'application/json',
-            ...headers,
-        };
-        const response: AxiosResponse<T> = await this.axiosInstance.delete<T>(url, {
-            headers,
-        });
-        const responseHeaders = this.convertHeaders(response.headers);
-
-        return new ResponseObject<T>(responseHeaders, response.data);
-    }
-    
-    async patch<T>(url: string, data: any, headers: Record<string, string> = {}): Promise<ResponseObject<T>> {
-        headers = {
-            ...this.defaultHeaders,
-            'Content-Type': 'application/json',
-            ...headers,
-        };
-        const response: AxiosResponse<T> = await this.axiosInstance.patch<T>(url, data, {
-            headers,
-        });
-        const responseHeaders = this.convertHeaders(response.headers);
-
-        return new ResponseObject<T>(responseHeaders, response.data);
+        return this.request<T>('DELETE', url, undefined, headers);
     }
 }
